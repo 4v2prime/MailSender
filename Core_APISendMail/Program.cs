dd7d13b9926ae539806674121b33421c571f59b6
Prime-Property <vishalpardeshi8830@gmail.com>
2025-02-17 11:03:40 +0530

    program.cs updated

--- Core_APISendMail/Program.cs
+++ Core_APISendMail/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://*:{port}");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
